3f8e1b57781f77a662481f9afa151dbbeb4357aa
Rommel Rico <me@rommelrico.com>
2017-10-13 09:24:25 -0700

    testEqualOneCharacterStrings

--- LearningXcodeCodeCoverageTests/LearningXcodeCodeCoverageTests.swift
+++ LearningXcodeCodeCoverageTests/LearningXcodeCodeCoverageTests.swift
@@ -11,26 +11,14 @@ import XCTest
 
 class LearningXcodeCodeCoverageTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testEqualOneCharacterStrings() {
+        var myVC: ViewController!
+
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle(for: ViewController.self))
+        myVC = storyboard.instantiateViewController(withIdentifier: "viewControllerID") as! ViewController
+        myVC.beginAppearanceTransition(true, animated: false)
+
+        XCTAssert(myVC.checkWord(word: "a", isAnagramOfWord: "a"))
     }
     
 }
